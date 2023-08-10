c4cf506042e6c7d4ac46fd241fff2cb5920b0a5c
Aurelius Wendelken <webtobesocial@gmail.com>
2023-08-10 08:14:38 +0200

    add tables from sozialatlas 2022 update sql queries

--- data/flensburg_31-40.sql
+++ data/flensburg_31-40.sql
@@ -1,15 +1,16 @@
-/**WENN NULL dann wert unter 4 -> datenschutz*/
+/* HINWEIS, WENN NULL DANN WERT UNTER 4 -> DATENSCHUTZ */
 
 
-/*NICHT_DEUTSCHE AUFENTHALTSSTATUS**/
+/* TABELLE 10, NICHT DEUTSCHE NACH AUFENTHALTSSTATUS */
 CREATE TABLE IF NOT EXISTS non_german_nationals_residence_status (
+  "id" SERIAL PRIMARY KEY,
 	"year" INT,
-	"langfristiges Aufenthaltsrech" INT,
-	"langfristiges Aufenthaltsrecht EU-Freizügikeitsgesetz" INT,
+	"langfristiges Aufenthaltsrecht" INT,
+	"langfristiges Aufenthaltsrecht nach EU-Freizügikeitsgesetz" INT,
 	"langfristiges Aufenthaltsrecht Drittstaatenangehörige" INT,
 	"ohne langfristiges Aufenthaltsrecht" INT,
-	"AsylbewerberInnen" INT,
-	"Aussetzung der Abschiebung" INT, 
+	"Asylbewerber:innen" INT,
+	"Aussetzung der Abschiebung" INT
 );
 
 INSERT INTO non_german_nationals_residence_status VALUES 
@@ -18,27 +19,27 @@ INSERT INTO non_german_nationals_residence_status VALUES
 (2018, 8363, 6865, 1498, 3987, 366, 214),
 (2019, 8117, 6569, 1548, 4585, 287, 275),
 (2020, 9510, 7842, 1668, 4747, 204, 252),
-(2021, 10178, 8314, 1864, 4868, 143, 276),
+(2021, 10178, 8314, 1864, 4868, 143, 276);
 
 
 
-/*ARBEITSLOSE 18-65**/
-CREATE TABLE IF NOT EXISTS unemployed (	
-    `district` int(11),
-    `residents` INT,
-    `2017` INT,
+/* TABELLE 12 ANTEIL ARBEITSLOSE 18 bis 65 */
+CREATE TABLE IF NOT EXISTS unemployed_residents (
+  "id" SERIAL PRIMARY KEY,
+  `district` INT,
+  `residents` INT,
+  `2017` INT,
 	`2018` INT,
 	`2019` INT,
 	`2020` INT,
 	`2021` INT
 );
 
-INSERT INTO unemployed VALUES 
-('Altstadt',233, 227, 218, 252, 226),
+INSERT INTO unemployed_residents VALUES ('Altstadt',233, 227, 218, 252, 226),
 ('Neustadt',390, 360, 343, 450, 383),
 ('Nordstadt',933, 856, 754, 927),
 ('Westliche Höhe',281, 259, 270, 350, 304),
-('Friesischer Berg',300, 276, 251, 288,286),
+('Friesischer Berg',300, 276, 251, 288, 286),
 ('Weiche',229, 204, 202, 230, 205),
 ('Südstadt',241, 239, 213, 238,226),
 ('Sandberg',292, 304, 296, 359, 327),
@@ -49,11 +50,13 @@ INSERT INTO unemployed VALUES
 ('Tarup',52, 53, 54, 98, 85);
 
 
-/*WOHNGELDEMPFÄNGERINNEN**/
+
+/* TABELLE 14 WOHNGELDEMPFÄNGERINNEN */
 CREATE TABLE IF NOT EXISTS housing_benefit (
+  "id" SERIAL PRIMARY KEY,
 	`district` TEXT,
-    `residents` INT,
-    `2011` INT,
+  `residents` INT,
+  `2011` INT,
 	`2017` INT,
 	`2018` INT,
 	`2019` INT,
@@ -61,8 +64,7 @@ CREATE TABLE IF NOT EXISTS housing_benefit (
 	`2021` INT
 );
 
-INSERT INTO housing_benefit VALUES 
-('Altstadt',130,96, 133, 119, 92, 115),
+INSERT INTO housing_benefit VALUES ('Altstadt',130,96, 133, 119, 92, 115),
 ('Neustadt',207,114, 165, 218, 172, 172),
 ('Nordstadt',584,413, 165, 571, 722, 523),
 ('Westliche Höhe',228,194, 212, 180, 189, 252),
@@ -74,24 +76,24 @@ INSERT INTO housing_benefit VALUES
 ('Fruerlund',337,360, 375, 416, 455, 370),
 ('Mürwik',623,504, 590, 569, 601, 484),
 ('Engelsby',392,233, 286, 228, 285, 247),
-('Tarup',91,44, 71, 56, 125, 114),
+('Tarup',91,44, 71, 56, 125, 114);
 
 
 
-/**WOHNUNGSHILFEFÄLLE TABELLE 15**/
+/* TABELLE 15 WOHNUNGSHILFEFÄLLE 2021 */
 CREATE TABLE IF NOT EXISTS housing_assistance_cases (
-    `district` TEXT,
-    `general_consulting` INT,
+  "id" SERIAL PRIMARY KEY,
+  `district` TEXT,
+  `general_consulting` INT,
 	`notices_of_rent_arrears` INT,
-	`termination__rent_arrears` INT,
-	`termination__for_conduct` INT,
+	`termination_rent_arrears` INT,
+	`termination_for_conduct` INT,
 	`action_for_eviction` INT,
 	`eviction_notice` INT,
-	`eviction_carried ` INT,
+	`eviction_carried` INT
 );
 
-INSERT INTO housing_benefit VALUES 
-('Altstadt', 25, 15, null, null, null, null, 5, 5), 
+INSERT INTO housing_benefit VALUES ('Altstadt', 25, 15, null, null, null, null, 5, 5), 
 ('Neustadt', 62, 39, null, 4, null, 5, 9, 5),
 ('Nordstadt', 243, 175, null, 19, 4, 15, 20, 10),
 ('Westliche Höhe', 83, 60, null, null, null, 11, 7, 5),
@@ -107,10 +109,10 @@ INSERT INTO housing_benefit VALUES
 
 
 
-/**WOHNUNGSLOSIGKEIT BEDROHTE TABELLE 16**/
+/* TABELLE 16 WOHNUNGSLOSIGKEIT BEDROHTE */
 CREATE TABLE IF NOT EXISTS households_at_risk_of_homelessness (
-    `district` TEXT,
-    `residents` INT,
+  `district` TEXT,
+  `residents` INT,
 	`2017` INT,
 	`2018` INT,
 	`2019` INT,
@@ -118,8 +120,7 @@ CREATE TABLE IF NOT EXISTS households_at_risk_of_homelessness (
 	`2021` INT
 );
 
-INSERT INTO households_at_risk_of_homelessness VALUES 
-('Altstadt', 14, 5, 13, 11, null), 
+INSERT INTO households_at_risk_of_homelessness VALUES ('Altstadt', 14, 5, 13, 11, null), 
 ('Neustadt', 21, 23, 21, 20, 5),
 ('Nordstadt', 31, 27, 27, 21, 15),
 ('Westliche Höhe', 8, 11,11,9,11),
@@ -131,13 +132,4 @@ INSERT INTO households_at_risk_of_homelessness VALUES
 ('Fruerlund', 6, 9,15,8,null),
 ('Mürwik', 28, 12, null, 21, 10),
 ('Engelsby', 17, 9, 14, 11, 12),
-('Tarup', null, null, null,  4, null),
-
-
-
-
-
-
-
-
-
+('Tarup', null, null, null, 4, null)
