377e31cb1ec7bdc83508c6a228505a0c34fd2ccb
Aurelius Wendelken <webtobesocial@gmail.com>
2023-08-22 16:31:12 +0200

    finally got what we need

--- generate_documentation.py
+++ generate_documentation.py
@@ -3,6 +3,8 @@
 import os
 import psycopg2
 
+from tabulate import tabulate
+
 
 conn = psycopg2.connect(
     database = os.environ.get('DB_NAME'),
@@ -11,6 +13,15 @@ conn = psycopg2.connect(
     host = os.environ.get('DB_HOST'),
     port = os.environ.get('DB_PORT')
 )
+
+
+def query_table(cur, table):
+    cur.execute(f'SELECT * FROM {table}')
+
+    rows = cur.fetchall()
+
+    field_names = [i[0] for i in cur.description]
+    print(tabulate(rows, headers=field_names, tablefmt='psql'))
 
 
 def main():
@@ -28,9 +39,7 @@ def main():
     rows = cur.fetchall()
 
     for row in rows:
-        tables.append(row[0])
-
-    print(tables)
+        query_table(cur, row[0])
 
 
 if __name__ == '__main__':
